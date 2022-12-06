49866c58f8301f6574617958b2edb83454bf72ad
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-06 20:58:23 +0300

    uptodate

---
+++ dz5/1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Mass(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0;i<n;i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array[i]} ");
+    }
+    int sum1 = 0;
+    for (int i = 0;i<n;i++)
+    {
+        if ((array[i] % 2) == 0)
+        {
+            sum1 = sum1 + 1;
+        } 
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Суума чет = {sum1}");
+}
+Console.WriteLine("Введите кол во");
+int s = int.Parse(Console.ReadLine());
+Mass(s);
---
+++ dz5/1.3/Program.cs
@@ -0,0 +1,32 @@
+﻿void Mass(int n)
+{   
+    Console.WriteLine("Введите числа");
+    double[] array = new double[n];
+    for (int i = 0;i<n;i++)
+    {
+        array[i] = double.Parse(Console.ReadLine());
+    }
+    for (int i = 0;i<n;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0;i<n;i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        } 
+        else if (array[i] < min)
+        {
+            min = array[i];
+        } 
+    }
+    Console.WriteLine();
+    double result = Math.Round((max-min),3);
+    Console.WriteLine($"Max - min = {result}");
+}
+Console.WriteLine("Введите кол во");
+int s = int.Parse(Console.ReadLine());
+Mass(s);
